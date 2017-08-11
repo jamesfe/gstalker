648be92987eb20d7a0f8603fd1165ce32e364a1a
jamesfe <james.ferrara@gmail.com>
2017-08-11 22:52:44 +0200

    Fixups

--- gstalker/database.py
+++ gstalker/database.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from copy import copy
-import json
 
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy_utils import database_exists, create_database
 
 
 def engine(config):
+    url = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}'
     engine = create_engine(url.format(**config), pool_size=1)
     if not database_exists(engine.url):
         try:
             temp_config = copy(config)
-            temp_config['user'] = config.get('admin_user', 'postgres')
-            temp_config['password'] = config.get('admin_password', 'postgres')
+            temp_config['user'] = config.get('user', 'postgres')
+            temp_config['password'] = config.get('password', 'postgres')
             temp_engine = create_engine(url.format(**temp_config), pool_size=1)
             create_database(temp_engine.url)
         except IntegrityError:
