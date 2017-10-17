c73e3fa3808afef3f2832d3a0675600109602aec
jamesfe <james.ferrara@gmail.com>
2017-10-17 22:13:10 +0200

    aws install

--- aws/install.sh
+++ aws/install.sh
@@ -4,3 +4,14 @@ sudo vim /var/lib/pgsql9/data/pg_hba.conf
 # modify a line to: local/all/all/trust
 # start service
 sudo service postgresql start
+
+sudo yum install git
+cd ~
+git clone https://github.com/jamesfe/gstalker.git
+cd gstalker
+sudo yum -y install python36
+python3 -m venv --without-pip localenv
+python3 -m pip install -r requirements.txt
+mkdir config
+vim config/config.json
+source ./localenv/bin/activate
